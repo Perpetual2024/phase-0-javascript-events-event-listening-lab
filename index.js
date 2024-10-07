40ed9cac4a08e84c24cb82d5ab8837188f6dc999
Perpetual Akinyi <you@example.com>
2024-10-07 16:25:32 +0300

    adding events

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(btn) {
+    const input =  document.getElementById('button');
+    input.addEventListener ('click',btn );
+    return input;
+        
+        
+    
+
 }
